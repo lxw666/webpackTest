191756286f1e22927b478123639adb0291088c78
lxw666 <1173442657@qq.com>
2020-06-09 18:11:08 +0800

    资源管理-加载图片

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import _ from 'lodash';
-function component(){
+import './index.css';
+import icon from './img/hot_icon.png';
+
+function component() {
 	var ele = document.createElement('div');
+	var img = new Image();
+	console.log(icon)
+	img.src = icon;
 	ele.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	ele.classList.add('font-color');
+	ele.appendChild(img);
 	return ele;
 }
 document.body.appendChild(component());
+/* "explorer.file.status.modified": "orange",
+		"explorer.file.status.untracked": "gray",
+		"explorer.file.status.added": "greeen",
+		"explorer.file.status.conflicted": "red" */
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
 const path = require('path');
-console.log(path,__dirname)
+console.log(path, __dirname)
 const config = {
 	entry: './src/index.js',
 	output: {
-		path: path.resolve(__dirname,'./dist/js'),
+		path: path.resolve(__dirname, './dist'),
 		filename: 'main.bundle.js'
+	},
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: [
+				'style-loader',
+				'css-loader',
+			]
+		},{
+			test: /\.(png|jpg|svg|gif)$/,
+			use: [
+				'file-loader'
+			]
+		}]
 	}
 };
 module.exports = config;
